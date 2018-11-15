6446d31a48e291e95cc7a8c60aa233502b9b5b8d
LFichefet <44224231+LFichefet@users.noreply.github.com>
2018-11-15 14:42:26 +0100

    Add files via upload

--- GT.m
+++ GT.m
@@ -9,12 +9,12 @@ function [ETA, DATEN, DATEX, DAT, MASSFLOW, COMBUSTION, FIG] = GT(P_e,options,di
 %         be activated)
 % P_E = electrical power output target [kW] OPTIONS is a structure
 % containing :
-%   -options.k_mec [-] : Shaft losses -options.T_0   [Â°C] : Reference
-%   temperature -options.T_ext [Â°C] : External temperature -options.r
+%   -options.k_mec [-] : Shaft losses -options.T_0   [°C] : Reference
+%   temperature -options.T_ext [°C] : External temperature -options.r
 %   [-] : Comperssion ratio -options.k_cc  [-] : Coefficient of pressure
 %   losses due to combustion
 %                        chamber
-%   -options.T_3   [Â°C] : Temperature after combustion (before turbine)
+%   -options.T_3   [°C] : Temperature after combustion (before turbine)
 %   -option.eta_PiC[-] : Intern polytropic efficiency (Rendement
 %                        polytropique interne) for compression
 %   -option.eta_PiT[-] : Intern polytropic efficiency (Rendement
@@ -35,7 +35,7 @@ function [ETA, DATEN, DATEX, DAT, MASSFLOW, COMBUSTION, FIG] = GT(P_e,options,di
 %   -datex(1) : perte_mec [kW] -datex(2) : perte_rotex [kW] -datex(3) :
 %   perte_combex [kW] -datex(4) : perte_echex  [kW]
 % DAT is a matrix containing : dat = {T_1       , T_2       , T_3       ,
-% T_4; [Â°C]
+% T_4; [°C]
 %        p_1       , p_2       , p_3       , p_4; [bar] h_1       , h_2
 %        , h_3       , h_4; [kJ/kg] s_1       , s_2       , s_3       ,
 %        s_4; [kJ/kg/K] e_1       , e_2       , e_3       , e_4;};[kJ/kg]
@@ -86,7 +86,7 @@ for d=1
     if isfield(options,'T_0')
         T0 = options.T_0;
     else
-        T0 = 0; % [Â°C]  
+        T0 = 0; % [°C]  
     end
     if isfield(options,'k_mec')
         k_mec = options.k_mec;
@@ -96,7 +96,7 @@ for d=1
     if isfield(options,'T_ext')
         T_ext = options.T_ext;
     else
-        T_ext = 15; % [Â°C]  
+        T_ext = 15; % [°C]  
     end
     if isfield(options,'k_cc')
         k_cc = options.k_cc;
@@ -111,7 +111,7 @@ for d=1
     if isfield(options,'T_3')
         T3 = options.T_3;
     else
-        T3 = 1050; % [Â°C] voir enonce  
+        T3 = 1050; % [°C] voir enonce  
     end
     if isfield(options,'eta_PiC')
         eta_PiC = options.eta_PiC;
@@ -152,7 +152,7 @@ T0 = T0+273.15;
 
 % Etat de reference
 [Cpa_0,Ra] = Cpa(T0);
-h0 = Cpa_0*(T0-273.15); % Par rapport a 0Â°C (p.118)
+h0 = Cpa_0*(T0-273.15); % Par rapport a 0°C (p.118)
 s0 = Cpa_0*log(T0/273.15);
 
 %% Etat 1 : Arrivee de l'air
@@ -198,7 +198,7 @@ T(3) = T3+273.15; % [K]
 %     Cpg(L,T(3),T(2))/Cpa(T(2),T(1)) - exp) - LHV/(L*ma1);
 %f = @(L) (1+LHV/((L*ma1+1)*Cpg(L,T(3),T(2))*T(2)))-(T(3)/T(2));
 lambda = fsolve(@ComputeLambda,2.5,opt);
-[MO2, MCO2, MN2, MH2O, Rg] = ComputeMassFraction(lambda);
+[MO2, MCO2, MN2, MH2O, Rg] = ComputeMassFraction(lambda,x,y);
 MF = [MO2 MCO2 MN2 MH2O];
 
 % On calcule le reste de l'etat
@@ -299,111 +299,52 @@ if display == 1
     Tdraw = [T12 T23 T34];
     FIG(1) = figure; % h-s
     plot(sdraw,hdraw);
-    title('Diagramme h-s de la turbine Ã  gaz')
+    title('Diagramme h-s de la turbine à gaz')
     xlabel('Entropie [kJ/kg/K]')
     ylabel('Enthalpie [kJ/kg]')
     FIG(2) = figure;
     plot(sdraw, Tdraw); % T-s
-    title('Diagramme T-s de la turbine Ã  gaz')
+    title('Diagramme T-s de la turbine à gaz')
     xlabel('Entropie [kJ/kg/K]')
-    ylabel('TempÃ©rature [Â°K]')
+    ylabel('Température [°K]')
     FIG(3) = figure; % Energie
-    legend = {sprintf('Pertes a l''Ã©chappement \n %.1f MW',Pechen/1000), ...
-        sprintf('Pertes mÃ©caniques \n %.1f MW',Pmec/1000), ...
+    legend = {sprintf('Pertes a l''échappement \n %.1f MW',Pechen/1000), ...
+        sprintf('Pertes mécaniques \n %.1f MW',Pmec/1000), ...
         sprintf('Puissance effective \n %.1f MW',P_e/1000)};
     pie([Pechen Pmec P_e],legend);
-    title('Flux d''Ã©nergie de la turbine Ã  gaz');
+    title('Flux d''énergie de la turbine à gaz');
     FIG(4) = figure; % Exergie
-    legend = {sprintf('IrrÃ©versibilitÃ© de la combustion \n %.1f MW',Pcombex/1000),...
-        sprintf('Pertes mÃ©caniques \n %.1f MW',Pmec/1000), ...
-        sprintf('Pertes Ã  l''Ã©chappement \n %.1f MW',Pechex/1000), ...
-        sprintf('IrrÃ©versibilitÃ©s turb. et compr. \n %.1f MW',Protex/1000)...
+    legend = {sprintf('Irréversibilité de la combustion \n %.1f MW',Pcombex/1000),...
+        sprintf('Pertes mécaniques \n %.1f MW',Pmec/1000), ...
+        sprintf('Pertes à l''échappement \n %.1f MW',Pechex/1000), ...
+        sprintf('Irréversibilités turb. et compr. \n %.1f MW',Protex/1000)...
         sprintf('Puissance effective \n %.1f MW',P_e/1000)};
     pie([Pcombex Pmec Pechex Protex P_e],legend);  
-    title('Flux d''exergie de la turbine Ã  gaz');
+    title('Flux d''exergie de la turbine à gaz');
 end
 
 
 %% NESTED FUNCTIONS
-% Cpa : Fonction qui renvoie une chaleur specifique de l'air a la temp. T
-% et peut aussi donner sa constante des gazs, R*.
-% Si nargout == 2, retourne aussi la constante R* de l'air [kJ/kg/K]
-% Si nargin == 1, retourne la chaleur specifique a la TÂ° demandee
-% Si nargin == 2, retourne la chaleur specifique moyennee entre T1 et T2
-function [Cpa_m, Ra] = Cpa(T2,T1)
 
-    Ma = 0.79*MmN2 + 0.21*MmO2; % Masse molaire de l'air 
 
-    %Fraction massique de l'air [-]
-    MO2 = MmO2*0.21/Ma;
-    MN2 = MmN2*0.79/Ma;
-    if nargin ==2
-        Tvec = linspace(T1,T2,100);
-    elseif nargin ==1
-        Tvec = T2;
-    end
-    Cpa_vec = janaf('c','O2',Tvec)*MO2+janaf('c','N2',Tvec)*MN2;
-    if nargin == 2
-        Cpa_m = sum(Cpa_vec)/length(Tvec);
-    else
-        Cpa_m = Cpa_vec;
-    end
+
+% Trouve lambda de façon iterative, en prenant la definition de Qcomb
+% Equation (3.9) p.115
+function f = ComputeLambda(L)
+    [MO2, MCO2, MN2, MH2O, Rg] = ComputeMassFraction(L,x,y);
+    MassFr = [MO2 MCO2 MN2 MH2O];
     
-    if nargout > 1
-        R = 8.314462*1e-3; % [kJ/mol/K]
-        Ra = R/Ma;
-    end
-end
-
-% Renvoie la chaleur specifique des fumees a T2 ou moyennee entre T2 et T1.
-% M est un vecteur : M(1) = MO2 // M(2) = MCO2 // M(3) = MN2 // M(4) = MH2O
-function [Cpg_m] = Cpg(M,T2,T1)
+    Tvec = linspace(T(3),T(2),100);
     
-    if nargin == 3
-        Tvec = linspace(T1,T2,100); % Travail avec un vecteur
-    elseif nargin == 2
-        Tvec = T2; % Chaleur spec. a un point donne
-    end
     CpO2 = janaf('c','O2',Tvec);
     CpCO2 = janaf('c','CO2',Tvec);
     CpN2 = janaf('c','N2',Tvec);
     CpH2O = janaf('c','H2O',Tvec);
-    Cpg_vec = M(1)*CpO2 + M(2)*CpCO2 + M(3)*CpN2 + M(4)*CpH2O; % Vecteur ou scalaire selon T
-    if nargin == 3
-        Cpg_m = sum(Cpg_vec)/length(Tvec);
-    elseif nargin == 2
-        Cpg_m = Cpg_vec;
-    end    
+    Cpg_vec = MassFr(1)*CpO2 + MassFr(2)*CpCO2 + MassFr(3)*CpN2 + MassFr(4)*CpH2O; % Vecteur ou scalaire selon T
+    
+    Cpg_m = sum(Cpg_vec)/length(Tvec);
+
+    f = Cpg_m*(T(3)-T(2))+(1/(L*ma1))*(Cpg_m*(T(3)-T0)-LHV);
 end
 
-function f = ComputeLambda(L)
-    [MO2, MCO2, MN2, MH2O, Rg] = ComputeMassFraction(L);
-    MassFr = [MO2 MCO2 MN2 MH2O];
-    Cpg = Cpg(MassFr,T(3),T(2));
-    f = Cpg*(T(3)-T(2))+(1/(L*ma1))*(Cpg*(T(3)-T0)-LHV);
-end
-
-function [MO2, MCO2, MN2, MH2O, Rg] = ComputeMassFraction(L)
-    
-    % Fraction molaire
-    A = (y-2*x)/4; % Simplicite d'ecriture
-    denom = y/2 + (L-1)*(1+A)+3.76*L*(1+A);
-    O2 = (L-1)*(1+A)/denom;
-    CO2 = 1/denom;
-    N2 = 3.76*L*(1+A)/denom;
-    H2O = (y/2)/denom;
-    
-    % Masse molaire des fumees
-    M_g = MmO2*O2+MmCO2*CO2+MmN2*N2+MmH2O*H2O; % kg_g/mol_g
-    if nargout > 4
-        R = 8.314462*1e-3; % [kJ/mol/K]
-        Rg = R/M_g; % [kJ/kg/K]
-    end
-    % fraction massique
-    MO2 = MmO2*O2/M_g;
-    MCO2 = MmCO2*CO2/M_g;
-    MN2 = MmN2*N2/M_g;
-    MH2O = MmH2O*H2O/M_g;
-    
-end
 end
