20361fa0a2e96797be36c23a5b3f3c80729eb92a
augustincollie <44058888+augustincollie@users.noreply.github.com>
2018-12-11 09:07:32 +0100

    Update CoolingTower.m

--- CoolingTower.m
+++ CoolingTower.m
@@ -64,27 +64,29 @@ end
 end
 
 %%Importation des données
-for d=1
+for d=1 %Only to allow collapsing of the code...
     if isfield(options,'Tcond')
         Tcond = options.Tcond;
     else
-        Tcond = 33;  % [C]
+        Tcond = 46.8;  % [C]
     end
    
     if isfield(options,'Tw_out')
+        if (options.Tw_out > (Tcond-Tpinch))
         Tw_out = options.Tw_out;
+        end
     else
         Tw_out = Tcond-Tpinch; %42.8;  % [C]
     end
     if isfield(options,'Tw_in')
         Tw_in = options.Tw_in;
     else
-        Tw_in = 25;  % [C]
+        Tw_in = 20;  % [C]
     end
     if isfield(options,'Triver')
         Triver = options.Triver;
     else
-        Triver = 10;  % [C]
+        Triver = 15;  % [C]
     end
     if isfield(options,'Ta_in')
         Ta_in = options.Ta_in;
@@ -99,7 +101,7 @@ for d=1
     if isfield(options,'Phi_atm')
         Phi_atm= options.Phi_atm;
     else
-        Phi_atm = 0.8;  % [-]
+        Phi_atm = 0.7;  % [-]
     end
     if isfield(options,'Phi_out')
         Phi_out = options.Phi_out;
@@ -118,6 +120,74 @@ end
     Cpv = 1.854; % chaleur specifique vapeur d'eau [kJ/kgK]
     Cpl = 4.1868; % chaleur specifique eau liquide [kJ/kgK]
     hLV = 2.5016; % enthalpie d'evaporation de l'eau [kJ/kg]
+    
+    
+%% LAST TRY ! 
+    % Given outputs
+    T_e2 = Tw_in; %[C]
+    T_e3 = Tw_out;
+    T_e4 = Triver;
+    
+    h_e2 = Cpl*T_e2; %[kJ/kg]
+    h_e3 = Cpl*T_e3;
+    h_e4 = Cpl*T_e4;
+    
+    m_e3 = P_w/(h_e3-h_e2); %[kg/s]
+    m_e2 = m_e3;
+    
+    Ta_1 = Ta_in; %[C]
+    Ta_2 = Ta_out;
+    
+    Phia_in = Phi_atm; %[-]
+    Phia_out = Phi_out;
+    
+    %States of cooling Air - Psychrometrics
+    [~, xa_in, ~, ha_in, ~, ~, ~] = Psychrometrics('Tdb',Ta_1,'phi',Phia_in);
+    [~, xa_out, ~, ha_out, ~, ~, ~] = Psychrometrics('Tdb',Ta_2,'phi',Phia_out);
+    ha_in = ha_in*1e-03;
+    ha_out = ha_out*1e-03;
+    
+    %System 3 Eq - 3 Unkn.
+    
+    function Inc = FunS(X)
+       Inc(1) = X(3)-(X(1)*(xa_out-xa_in)/(1+xa_in)) %Mass Conservation
+       
+       Term21 = m_e3*(h_e3-Cpl*X(2));
+       Term22 = X(1)*((ha_out-ha_in)-(Cpl*X(2)*(xa_out-xa_in)))/(1+xa_in);
+       Inc(2) = Term21 - Term22 %Enthalpy conservation at cooling tower
+       
+       Inc(3) = X(2)-((m_e3*h_e2-X(3)*h_e4)/((m_e3*X(3))*Cpl)) %Enthalpy conservation in water between 3 and 1
+    end
+
+    
+    Inc = ones(1,3); %Vectors of 3 unknowns of the system; [mDotAir, T_e1, m_e4]
+    Init = 1e04*Inc; %Initial iteration values of Inc
+    
+%     options = optimoptions('fsolve','OptimalityTolerance',1e-04);
+    Ans = fsolve(@FunS, Init); %,options
+    
+    mDotAir = Ans(1);
+    T_e1 = Ans(2);
+    m_e4 = Ans(3);
+    
+    m_e1 = m_e3-m_e4;
+    h_e1 = Cpl*T_e1;
+    
+    %Preparing Outputs
+    
+    MASSFLOW = [m_e3, m_e4, mDotAir] %[kg/s]
+          
+    DAT_WATER = [ T_e1       , T_e2       , T_e3       , T_e4;      %[°C]
+                  h_e1       , h_e2       , h_e3       , h_e4;      %[kJ/kg]
+                  m_e1       , m_e2       , m_e3       , m_e4 ]    %[kg/s]
+    DAT_AIR   = [ Ta_in       , Ta_out ;     %[°C]
+                  ha_in       , ha_out ;     %[kJ/kg]
+                  xa_in       , xa_out ;     %[kg_water/kg_dry_air]
+                  Phia_in     , Phia_out ]  %[-] relative humidity
+
+    
+    
+%% Anciennes méthodes.. A SUPPRIMER    
 %Evaluation du flux massique d'eau
     dhCond = Cpl*(Tw_out - Tw_in); % [kJ/kg]
     MASSFLOW(1) = P_w/dhCond % [kg/s]
